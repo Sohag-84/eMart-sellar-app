de57877b8659fcdca3cbbd7d96f9ea4a2f9044a8
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-11 21:22:06 +0600

    show popular product

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_mart_seller/const/const.dart';
+import 'package:e_mart_seller/services/firestore_services.dart';
+import 'package:e_mart_seller/views/products/product_details.dart';
 import 'package:e_mart_seller/widgets/appbar_widget.dart';
 import 'package:e_mart_seller/widgets/dashboard_button.dart';
+import 'package:e_mart_seller/widgets/loading_indicator.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,75 +15,97 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbarWidget(title: dahsboard),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                dashboardButton(
-                  context: context,
-                  title: dahsboard,
-                  quantity: "79",
-                  icon: icProducts,
-                ),
-                dashboardButton(
-                  context: context,
-                  title: orders,
-                  quantity: "30",
-                  icon: icOrders,
-                ),
-              ],
-            ),
-            10.heightBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                dashboardButton(
-                  context: context,
-                  title: rating,
-                  quantity: "10",
-                  icon: icStar,
-                ),
-                dashboardButton(
-                  context: context,
-                  title: totalSells,
-                  quantity: "30",
-                  icon: icOrders,
-                ),
-              ],
-            ),
-            10.heightBox,
-            Divider(color: darkGrey),
-            10.heightBox,
-            boldText(text: popularProducts, color: fontGrey, size: 16),
-            20.heightBox,
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                physics: BouncingScrollPhysics(),
-                children: List.generate(
-                  10,
-                  (index) {
-                    return ListTile(
-                      leading: Image.asset(
-                        imgProduct,
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
+      body: StreamBuilder(
+          stream: FirestroeServices.getProducts(uid: currentUser!.uid),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return loadingIndicator();
+            } else {
+              var data = snapshot.data!.docs;
+              data = data.sortedBy(
+                (a, b) =>
+                    b['p_wishlist'].length.compareTo(a['p_wishlist'].length),
+              );
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        dashboardButton(
+                          context: context,
+                          title: dahsboard,
+                          quantity: "79",
+                          icon: icProducts,
+                        ),
+                        dashboardButton(
+                          context: context,
+                          title: orders,
+                          quantity: "30",
+                          icon: icOrders,
+                        ),
+                      ],
+                    ),
+                    10.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        dashboardButton(
+                          context: context,
+                          title: rating,
+                          quantity: "10",
+                          icon: icStar,
+                        ),
+                        dashboardButton(
+                          context: context,
+                          title: totalSells,
+                          quantity: "30",
+                          icon: icOrders,
+                        ),
+                      ],
+                    ),
+                    10.heightBox,
+                    Divider(color: darkGrey),
+                    10.heightBox,
+                    boldText(text: popularProducts, color: fontGrey, size: 16),
+                    20.heightBox,
+                    Expanded(
+                      child: ListView(
+                        shrinkWrap: true,
+                        physics: BouncingScrollPhysics(),
+                        children: List.generate(
+                          data.length,
+                          (index) {
+                            return ListTile(
+                              onTap: () {
+                                Get.to(() => ProductDetails(data: data[index]));
+                              },
+                              leading: Image.network(
+                                data[index]['p_images'][0],
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.fill,
+                              ),
+                              title: boldText(
+                                text: "${data[index]['p_name']}",
+                                color: fontGrey,
+                              ),
+                              subtitle: normalText(
+                                text: "\$ ${data[index]['p_price']}",
+                                color: darkGrey,
+                              ),
+                            );
+                          },
+                        ),
                       ),
-                      title: boldText(text: "Product title", color: fontGrey),
-                      subtitle: normalText(text: "\$40", color: darkGrey),
-                    );
-                  },
+                    ),
+                  ],
                 ),
-              ),
-            ),
-          ],
-        ),
-      ),
+              );
+            }
+          }),
     );
   }
 }
